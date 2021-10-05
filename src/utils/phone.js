71224df0896bdff2776ef7d79324a08eec2b0c81
labamnus <labamnus@gmail.com>
2021-10-05 22:24:03 +0600

    Move secrets to .env

--- src/utils/phone.js
+++ src/utils/phone.js
@@ -3,9 +3,9 @@ import fetch from 'node-fetch';
 export const sendPhoneVerify = async (phone) => {
     return (
         await fetch(
-            `https://api.ucaller.ru/v1.0/initCall?service_id=692777&key=NL5PauUIVrFdfx90pjXETpevg6TFjc7F&phone=${phone.slice(
-                1
-            )}`
+            `https://api.ucaller.ru/v1.0/initCall?service_id=${
+                process.env.SERVICE_ID
+            }&key=${process.env.SERVICE_KEY}&phone=${phone.slice(1)}`
         )
     ).json();
 };
